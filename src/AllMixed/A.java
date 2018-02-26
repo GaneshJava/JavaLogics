eb353edc4c92236c3ac50ee2f10e8e624c1b3840
B N S Ganesh Prasad <ganesh.java7@gmail.com>
2018-02-26 13:39:19 +0530

    Delete A.java

--- src/AllMixed/A.java
+++
@@ -1,9 +0,0 @@
-package AllMixed;
-class A extends Thread {
-	private int i;
-	public void run() {i = 2;}
-	public static void main(String[] args) {
-	A a = new A();
-	a.run();
-	System.out.print(a.i);
-	}}
